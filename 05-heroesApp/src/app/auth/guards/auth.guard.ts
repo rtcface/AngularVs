a8d6192c98970edd8733c152076755f3269fca3e
Ramiro Tepehua Cortes <rtcface@gmail.com>
2021-09-01 19:59:31 +0000

    Se implementa la proteccion de rutas hasta el canLoad

--- 05-heroesApp/src/app/auth/guards/auth.guard.ts
+++ 05-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -6,15 +6,15 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  CanLoad {
+export class AuthGuard implements  CanLoad, CanActivate {
   constructor(private authService: AuthService){
 
   };
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //   return true;
-  // }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
